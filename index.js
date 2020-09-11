28936443a6605f2aa24e6320415ceca1da1cbad3
kashifrazabb <kashifrazabb@gmail.com>
2020-09-12 03:57:05 +0500

    tree

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { data } from './test.js'
+
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index', { data })
+})
+
+
+app.listen(3000, () => console.log('Server is listening to port 3000'))
